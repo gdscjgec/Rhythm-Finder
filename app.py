ee27f6403db9bd83abb8714222279f79418280c7
Sagnik Roy <58679327+sagnik1511@users.noreply.github.com>
2021-10-01 00:31:48 +0530

    Update and rename main.py to app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+
+Rhythm-Finder app
+
+This file will take the user
+data and predict the songs.
+
+
+"""
+
+# Libraries ------
+
+import streamlit as st
+
+# libraries ------
+
+# Headers ------
+
+st.header('Rhythm-Finder App')
+
+st.subheader('GDSC-JGEC')
+
+# Headres ------
+
+
+# User Inputs ------
+
+# User Inputs ------
+
+
+# Model Load and prediction ------
+
+# Model Load and prediction ------
+
+
+# Footer ------
+
+# Footer ------
